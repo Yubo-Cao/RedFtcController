c7e5288902a6a92cedc0460e441a647e2247d570
Yubo-Cao <cao2006721@gmail.com>
2023-01-19 21:36:14 -0500

    feat(BasicDrive): fix android studio warnings

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicDrive.java
@@ -1,11 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Basic Drive X", group = "Iterative Opmode")
 public class BasicDrive extends OpMode {
